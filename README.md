5b2d837ff7b21939a5ff7a21be22058915de8931
ezekielswanson <swansonezekiel@gmail.com>
2024-06-23 19:58:09 -0500

    Updated files

---
+++ README.md
@@ -0,0 +1,7 @@
+Full stack application that allows users to create, update, or delete blog posts.
+
+Technologies used: Node.js, Express, MongoDB, EJS
+
+
+
+<img width="1196" alt="Screen Shot 2024-06-22 at 6 32 02 PM" src="https://github.com/ezekielswanson/blog-project-node/assets/75798690/09dbd147-6926-4904-a548-26d147f4de8f">
---
+++ controllers/blogController.js
@@ -0,0 +1,156 @@
+const Blog = require('../models/blog');
+
+const blog_index = (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('index', { blogs: result, title: 'All blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+const blog_details = (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch(err => {
+      console.log(err);
+      res.render('404', { title: 'Blog not found' });
+    });
+}
+
+const blog_create_get = (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+}
+
+const blog_create_post = (req, res) => {
+  const blog = new Blog(req.body);
+  blog.save()
+    .then(result => {
+      res.redirect('/blogs');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+const blog_delete = (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+module.exports = {
+  blog_index, 
+  blog_details, 
+  blog_create_get, 
+  blog_create_post, 
+  blog_delete
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* What file am I working with the model on? 
+Import that file here 
+*/
+
+/* Aside
+
+//importing blog model allowing you to put routes on the 
+//blog model
+//.. means go out of the current directory into another
+//code runs from top to bottom
+
+*/
+
+
+/*** New Code Here  ****/
+/*
+const Blog = require('../models/blog');
+
+const blog_index = (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('index', { blogs: result, title: 'All blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+const blog_details = (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch(err => {
+      console.log(err);
+      res.render('404', { title: 'Blog not found' });
+    });
+}
+
+
+//in node can't send redirect as response when sending ajax req
+    /*
+    -will send json data BACK to brwoser and that json data has redirect prop
+    -when data's recieved back in details.ejs look @ redirect prop 
+    and this will be where we redirect. Based on the redirect prop  in jSON
+*/
+
+/*
+const blog_create_get = (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+}
+
+const blog_create_post = (req, res) => {
+  const blog = new Blog(req.body);
+  blog.save()
+    .then(result => {
+      res.redirect('/blogs');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+const blog_delete = (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+//This makes blog_index available as a property of the exported object.
+module.exports = {
+  blog_index, 
+  blog_details, 
+  blog_create_get, 
+  blog_create_post, 
+  blog_delete
+}
+*/
